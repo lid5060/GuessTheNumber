2456a81af229c47f97c8f7001143b1bcdb39cbab
Kyle <kp3crt@bolton.ac.uk>
2024-02-05 12:42:57 +0000

    Changed the code and added in that the user now has to press enter for the programme to exit

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import random
 n = random.randint(1, 99)
-guess = int(input("Enter an integer from 1 to 99: "))
+guess = int(input("Enter a number from 1 to 99:"))
 while True:
     if guess < n:
-        print("guess is low")
-        guess = int(input("Enter an integer from 1 to 99: "))
+        print("guess is too low")
+        guess = int(input("Enter an number from 1 to 99:"))
     elif guess > n:
-        print("guess is high")
-        guess = int(input("Enter an integer from 1 to 99: "))
+        print("guess is too high")
+        guess = int(input("Enter a number from 1 to 99:"))
     else:
-        print("you guessed it right! Bye!")
+        print("you guessed it right!")
         break
+input("press enter to exit")
